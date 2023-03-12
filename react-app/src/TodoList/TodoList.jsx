104839a4136b07fb94769996ad7ef62cc59c6180
AlfiyaKhazieva <khazieva.alfiya2020@mail.ru>
2023-03-12 15:38:31 +0300

    TodoList

--- react-app/src/TodoList/TodoList.jsx
+++ react-app/src/TodoList/TodoList.jsx
@@ -4,42 +4,59 @@ import { ReactComponent as Basket } from './basket.svg'
 import { ReactComponent as Added } from './added.svg'
 
 
-
-
+const initialState = [
+    { id: Math.random(), text: 'task1', status: 'active' },
+    { id: Math.random(), text: 'task2', status: 'active' },
+    { id: Math.random(), text: 'task3', status: 'active' },
+]
 
 function TodoList () {
 
+    // наш текст
     const [value, setValue] = useState(localStorage.getItem('value') || '');
-
+    
+    // наш туду, то есть список на страинце
     const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || []);
+    console.log(todos)
 
+    // функция, позволяющая писать текст в input
     const handleChange = (event) => {
         setValue(event.target.value)
-        //localStorage.setItem('value', value)     
     }
 
-    const handleClick = (event) => {
-        setTodos([...todos, value]);
+    // при клике на кнопку +, у нас отрисовывается список на странице
+    const handleClick = () => {
+        const valueTodo = {id: Math.random(), text: value, status: 'active'} 
+        // console.log(valueTodo);
+        setTodos([...todos, valueTodo]);
         setValue('');
-        localStorage.setItem('todos', JSON.stringify([...todos, value]))
+        localStorage.setItem('todos', JSON.stringify([...todos, valueTodo]))
     }
-
-    //useEffect(() => {
-        //localStorage.setItem('todos', JSON.stringify([...todos, value]))
-
-    //},[todos])
-
-    //useEffect(() => {
-        //setTodos(JSON.parse(localStorage.getItem('todos')) || [])
-
-    //},[])
 
     useEffect(() => {
         localStorage.setItem('value', value)
-
     },[value])
 
 
+    // изменение статуса (баскета и галочки)
+    const handleChangeStatus = (todo, status) => {
+       todo.status = status;
+       localStorage.setItem('todos', JSON.stringify(todos))
+       setTodos([...todos]) 
+    }
+
+    // меняем валью у селекта
+    const [status, setStatus] = useState(localStorage.getItem('status') || 'active');
+
+    const changeStatus = (e) => {
+        setStatus(e.target.value)
+    }
+
+    useEffect(() => {
+        localStorage.setItem('status', status)
+    },[status])
+
+    const results = todos.filter(todo => todo.status === status)
 
     return (
         <>
@@ -51,22 +68,22 @@ function TodoList () {
                     <button className='todoList__btn' onClick={handleClick} disabled={value === ''}><strong>+</strong></button>
                 </div>
                 <div className='todoList__conteiner'>
-                    <select className='todoList__btn-select'>
-                        <option value="">активные</option>
-                        <option value="">завершенные</option>
-                        <option value="">удаленные</option>
+                    <select defaultValue={status} className='todoList__btn-select' onChange={changeStatus}>
+                        <option value="active">активные</option>
+                        <option value="done">завершенные</option>
+                        <option value="deleted">удаленные</option>
                     </select>
                 </div>
             </div>
         </div>
-        <div className='tooList__todo'>
-            <ul className='todoList__list'>
-                {todos.map(function(todo){
+        <div className='todoList__todo'>
+            <ul className='todoList__list'> 
+                {results.map(function(result){
                     return (
-                       <li className='todoList__map' key={todo}>
-                        {todo} 
-                        <div className='todoList__basket'><Basket /></div> 
-                        <div className='todoList__added'><Added /></div> 
+                       <li className='todoList__map' key={result.id}>
+                        {result.text} 
+                        <div className='todoList__basket' onClick={() => handleChangeStatus(result, 'deleted')}><Basket/></div> 
+                        <div className='todoList__added' onClick={() => handleChangeStatus(result, 'done')}><Added /></div> 
                         </li>
                     )
                 })}
