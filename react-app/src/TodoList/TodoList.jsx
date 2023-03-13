5dbcf1ffde038c3ac278c697080523d0f2b68779
AlfiyaKhazieva <khazieva.alfiya2020@mail.ru>
2023-03-13 15:18:53 +0300

    TodoList-app

--- react-app/src/TodoList/TodoList.jsx
+++ react-app/src/TodoList/TodoList.jsx
@@ -6,22 +6,17 @@ import { ReactComponent as Added } from './added.svg'
 
 function TodoList () {
 
-    // наш текст
     const [value, setValue] = useState(localStorage.getItem('value') || '');
     
-    // наш туду, то есть список на страинце
     const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || []);
-    console.log(todos)
 
-    // функция, позволяющая писать текст в input
+
     const handleChange = (event) => {
         setValue(event.target.value)
     }
 
-    // при клике на кнопку +, у нас отрисовывается список на странице
     const handleClick = () => {
         const valueTodo = {id: Math.random(), text: value, status: 'active'} 
-        // console.log(valueTodo);
         setTodos([...todos, valueTodo]);
         setValue('');
         localStorage.setItem('todos', JSON.stringify([...todos, valueTodo]))
@@ -32,40 +27,43 @@ function TodoList () {
     },[value])
 
 
-    // изменение статуса (баскета и галочки)
+    // изменение статуса 
     const handleChangeStatus = (todo, status) => {
        todo.status = status;
        localStorage.setItem('todos', JSON.stringify(todos))
        setTodos([...todos]) 
     }
 
-    // меняем валью у селекта
+    // меняем статус у селекта
     const [status, setStatus] = useState(localStorage.getItem('status') || 'active');
 
     const changeStatus = (e) => {
         setStatus(e.target.value)
     }
 
+
     useEffect(() => {
         localStorage.setItem('status', status)
     },[status])
 
+
     const results = todos.filter(todo => todo.status === status)
+
 
     return (
         <>
         <div className='todoList'>
-            <h1 className='todoList__name'>Todo List</h1>
+            <h1 className='todoList__name'>ToDo List</h1>
             <div className='todoList__second'>
                 <div className='todoList__input-conteiner'>
-                    <input className='todoList__input' type="text" placeholder="Введите текст..." onChange={handleChange} value={value}/>
-                    <button className='todoList__btn' onClick={handleClick} disabled={value === ''}><strong>+</strong></button>
+                    <input className='todoList__input' type="text" maxLength={45} placeholder="Введите текст..." onChange={handleChange} value={value}/>
+                    <button className='todoList__btn' onClick={handleClick} disabled={value === ''}><strong className='send'>+</strong></button>
                 </div>
                 <div className='todoList__conteiner'>
                     <select defaultValue={status} className='todoList__btn-select' onChange={changeStatus}>
-                        <option value="active">активные</option>
-                        <option value="done">завершенные</option>
-                        <option value="deleted">удаленные</option>
+                        <option className='option' value="active">активные</option>
+                        <option className='option' value="done">завершенные</option>
+                        <option className='option' value="deleted">удаленные</option>
                     </select>
                 </div>
             </div>
@@ -76,8 +74,8 @@ function TodoList () {
                     return (
                        <li className='todoList__map' key={result.id}>
                         {result.text} 
-                        <div className='todoList__basket' onClick={() => handleChangeStatus(result, 'deleted')}><Basket/></div> 
-                        <div className='todoList__added' onClick={() => handleChangeStatus(result, 'done')}><Added /></div> 
+                        <div className='todoList__basket' onClick={() => handleChangeStatus(result, 'deleted')}><Basket className='basket'/></div> 
+                        <div className='todoList__added' onClick={() => handleChangeStatus(result, 'done')}><Added className='added' /></div> 
                         </li>
                     )
                 })}
